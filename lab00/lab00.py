dac469d30f45a5c8c8ede1a9d26934baa9701a9a
William Buerk <williambuerk@gmail.com>
2021-01-23 11:08:26 -0600

    lab00 final solution

--- lab00/lab00.py
+++ lab00/lab00.py
@@ -1,8 +1,10 @@
 # Change this function
 def my_palindrome():
-    return ''
+    return 'racecar'
 
 # Do not change the main method
+
+
 def main():
     pal = my_palindrome()
     assert isinstance(pal, str), 'Must return a string'
@@ -10,6 +12,7 @@ def main():
     assert len(pal) > 1, 'String should be at least 2 letters long'
     assert pal == pal[::-1], 'String is not a palindrome'
     print("your successfully created a palindrome!")
+
 
 if __name__ == '__main__':
     main()
