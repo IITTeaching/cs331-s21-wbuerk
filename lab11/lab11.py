689e3a79886dd55cd87d30c7ca932a854beb8313
William Buerk <williambuerk@gmail.com>
2021-05-08 18:14:56 -0500

    lab11 and radix-sort

--- lab11/lab11.py
+++ lab11/lab11.py
@@ -1,71 +1,137 @@
 from unittest import TestCase
 import random
 
-def quicksort(lst,pivot_fn):
-    qsort(lst,0,len(lst) - 1,pivot_fn)
 
-def qsort(lst,low,high,pivot_fn):
-    ### BEGIN SOLUTION
-    ### END SOLUTION
+def quicksort(lst, pivot_fn):
+    qsort(lst, 0, len(lst) - 1, pivot_fn)
 
-def pivot_first(lst,low,high):
-    ### BEGIN SOLUTION
-    ### END SOLUTION
 
-def pivot_random(lst,low,high):
-    ### BEGIN SOLUTION
-    ### END SOLUTION
+def qsort(lst, low, high, pivot_fn):
+    # BEGIN SOLUTION
+    if low < high:
+        print("start pivot")
+        print(f"low: {low}")
+        print(f"high: {high}")
+        p = pivot_fn(lst, low, high)
+        qsort(lst, low, p-1, pivot_fn)
+        qsort(lst, p+1, high, pivot_fn)
 
-def pivot_median_of_three(lst,low,high):
-    ### BEGIN SOLUTION
-    ### END SOLUTION
+    # END SOLUTION
 
-################################################################################
-# TEST CASES
-################################################################################
+
+def pivot_first(lst, low, high):
+    # BEGIN SOLUTION
+
+    lst[low], lst[high] = lst[high], lst[low]
+
+    pivot = lst[high]
+    i = low
+    for j in range(low, high):
+        if lst[j] < pivot:
+            lst[i], lst[j] = lst[j], lst[i]
+            i = i + 1
+    lst[i], lst[high] = lst[high], lst[i]
+    return i
+
+    # END SOLUTION
+
+
+def pivot_random(lst, low, high):
+    # BEGIN SOLUTION
+    rand = random.randint(low, high)
+    lst[rand], lst[high] = lst[high], lst[rand]
+
+    pivot = lst[high]
+    i = low
+    for j in range(low, high):
+        if lst[j] < pivot:
+            lst[i], lst[j] = lst[j], lst[i]
+            i = i + 1
+    lst[i], lst[high] = lst[high], lst[i]
+    return i
+    # END SOLUTION
+
+
+def pivot_median_of_three(lst, low, high):
+    # BEGIN SOLUTION
+    med_lst = sorted([lst[low], lst[(low + high) // 2], lst[high]])
+    med = med_lst[1]
+    med_idx = lst.index(med, low, high+1)
+    lst[med_idx], lst[high] = lst[high], lst[med_idx]
+
+    pivot = lst[high]
+    i = low
+    for j in range(low, high):
+        if lst[j] < pivot:
+            lst[i], lst[j] = lst[j], lst[i]
+            i = i + 1
+    lst[i], lst[high] = lst[high], lst[i]
+    return i
+    # END SOLUTION
+
+    ################################################################################
+    # TEST CASES
+    ################################################################################
+
+
 def randomize_list(size):
-    lst = list(range(0,size))
-    for i in range(0,size):
-        l = random.randrange(0,size)
-        r = random.randrange(0,size)
+    lst = list(range(0, size))
+    for i in range(0, size):
+        l = random.randrange(0, size)
+        r = random.randrange(0, size)
         lst[l], lst[r] = lst[r], lst[l]
     return lst
+
 
 def test_lists_with_pfn(pfn):
     lstsize = 20
     tc = TestCase()
-    exp = list(range(0,lstsize))
+    exp = list(range(0, lstsize))
 
-    lst = list(range(0,lstsize))
-    quicksort(lst, pivot_first)
-    tc.assertEqual(lst,exp)
+    lst = list(range(0, lstsize))
+    quicksort(lst, pfn)
+    tc.assertEqual(lst, exp)
 
-    lst = list(reversed(range(0,lstsize)))
-    quicksort(lst, pivot_first)
-    tc.assertEqual(lst,exp)
+    print("test 1 passed")
 
-    for i in range(0,100):
+    lst = list(reversed(range(0, lstsize)))
+    print(lst)
+    quicksort(lst, pfn)
+    tc.assertEqual(lst, exp)
+
+    print("test 2 passed")
+
+    for i in range(0, 1):
         lst = randomize_list(lstsize)
         quicksort(lst, pfn)
-        tc.assertEqual(lst,exp)
+        tc.assertEqual(lst, exp)
 
 # 30 points
+
+
 def test_first():
     test_lists_with_pfn(pivot_first)
 
 # 30 points
+
+
 def test_random():
     test_lists_with_pfn(pivot_random)
 
 # 40 points
+
+
 def test_median():
     test_lists_with_pfn(pivot_median_of_three)
 
 ################################################################################
 # TEST HELPERS
 ################################################################################
+
+
 def say_test(f):
     print(80 * "#" + "\n" + f.__name__ + "\n" + 80 * "#" + "\n")
+
 
 def say_success():
     print("----> SUCCESS")
@@ -73,7 +139,10 @@ def say_success():
 ################################################################################
 # MAIN
 ################################################################################
+
+
 def main():
+
     for t in [test_first,
               test_random,
               test_median]:
@@ -81,6 +150,7 @@ def main():
         t()
         say_success()
     print(80 * "#" + "\nALL TEST CASES FINISHED SUCCESSFULLY!\n" + 80 * "#")
+
 
 if __name__ == '__main__':
     main()
--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,10 +1,100 @@
 import urllib
 import requests
+from unittest import TestCase
+
 
 def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     booktxt = urllib.request.urlopen(book_url).read().decode()
-    bookascii = booktxt.encode('ascii','replace')
+    bookascii = booktxt.encode('ascii', 'replace')
     return bookascii.split()
 
+
+def max_length(lst):
+    max_len = 0
+
+    for word in lst:
+        length = len(word)
+        if length > max_len:
+            max_len = length
+
+    return max_len
+
+
+def rolling_sum(lst):
+
+    if len(lst) < 1:
+        raise ValueError
+
+    for x in range(1, len(lst)):
+        lst[x] += lst[x-1]
+
+    return lst
+
+
+def counting_sort(lst, index):
+    counting_list = [0] * 257
+    aux_list = [None] * len(lst)
+
+    for word in lst:
+        try:
+            # byte = ord(word[index])
+            byte = word[index]
+        except IndexError:
+            byte = -1
+
+        counting_list[byte + 1] += 1
+
+    counting_list = rolling_sum(counting_list)
+
+    for x in range(len(lst)-1, -1, -1):
+        word = lst[x]
+        try:
+            # byte = ord(word[index])
+            byte = word[index]
+        except IndexError:
+            byte = -1
+
+        idx = counting_list[byte+1]
+        aux_list[idx-1] = word
+        counting_list[byte+1] += -1
+
+    return aux_list
+
+
+def radix_sort(lst):
+    max_len = max_length(lst)
+
+    for x in range(max_len-1, -1, -1):
+        lst = counting_sort(lst, x)
+
+    return lst
+
+
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    return radix_sort(book_to_words(book_url))
+
+
+if __name__ == "__main__":
+
+    tc = TestCase()
+
+    lst = [b'asldjsdakflj', b'a', b'baby', b'zebra',
+           b'zany', b'apple', b'apply', b'apps', b'ashed']
+
+    radixed_list = radix_sort(lst)
+    sorted_list = sorted(lst)
+
+    tc.assertEqual(radixed_list, sorted_list)
+    print("#" * 80)
+    print("Test Case 1 Succesful!")
+    print("#" * 80)
+
+    tc = TestCase()
+
+    radixed_book = radix_a_book()
+    sorted_book = sorted(book_to_words())
+
+    tc.assertEqual(radixed_book, sorted_book)
+    print("#" * 80)
+    print("Radixed Book Succesfully!")
+    print("#" * 80)
